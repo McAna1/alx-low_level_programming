31e8d0590924270e79b93e6d66615612382d61b1
McAna1 <mcanachavez@gmail.com>
2022-06-03 09:19:04 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints sized of characters
+ * main - Prints sizes of characters
  *
- *Return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	longint longintType;
-	longlongint longlongintType;
-	float floatType;
-
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
