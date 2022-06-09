a7677b628335150d64e0495e70f8cfd641edb211
McAna1 <mcanachavez@gmail.com>
2022-06-09 11:09:08 -0700

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,18 +1,16 @@
 #include "main.h"
+
 /**
- * _isdigit - checks for a digit
- * @c: the parameter to print
- *
- * Return: 1 or 0
+ * _isdigit -  checks for checks for a digit (0 through 9).
+ * @c: c -  Variable
+ * Return: Always 0.
  */
 int _isdigit(int c)
 {
+
 	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	return (0);
 }
